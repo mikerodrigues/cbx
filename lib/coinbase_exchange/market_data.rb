40e14a8b240c3cb101b7b7e5203b5539ef055903
Michael Rodrigues <mikebrodrigues@gmail.com>
2015-03-14 21:11:09 -0700

    updated trading and market_data methods to accept blocks, passing results as arguments

--- lib/coinbase_exchange/market_data.rb
+++ lib/coinbase_exchange/market_data.rb
@@ -1,19 +1,19 @@
 class CoinbaseExchange
   module MarketData
-    def products
-      get('products')
+    def products(&block)
+      get('products', nil, &block)
     end
 
-    def orderbook(level='1', product_id='BTC-USD')
-      get('products/' + product_id + '/book?level=' + String(level))
+    def orderbook(level='1', product_id='BTC-USD', &block)
+      get('products/' + product_id + '/book?level=' + String(level), nil, &block)
     end
 
-    def ticker(product_id='BTC-USD')
-      get('products/' + product_id + '/ticker')
+    def ticker(product_id='BTC-USD', &block)
+      get('products/' + product_id + '/ticker', nil, &block)
     end
 
-    def trades(product_id='BTC-USD')
-      get('products/' + product_id + '/trades')
+    def trades(product_id='BTC-USD', &block)
+      get('products/' + product_id + '/trades', nil, &block)
     end
   end
 end
--- lib/coinbase_exchange/trading.rb
+++ lib/coinbase_exchange/trading.rb
@@ -2,61 +2,61 @@ class CoinbaseExchange
   module Trading
     # Account methods
     #
-    def accounts(account_id=nil)
+    def accounts(account_id=nil, &block)
       if account_id.nil? then
-        get('accounts')
+        get('accounts', nil, &block)
       else
-        get('accounts/' + account_id.to_s)
+        get('accounts/' + account_id.to_s, nil, &block)
       end
     end
 
-    def ledger(account_id, pagination = {})
-      get('accounts/' + account_id.to_s + '/ledger' + paginate(pagination))
+    def ledger(account_id, pagination = {}, &block)
+      get('accounts/' + account_id.to_s + '/ledger' + paginate(pagination), nil, &block)
     end
 
-    def holds(account_id, pagination = {})
-      get('accounts/' + account_id.to_s + '/holds' + paginate(pagination))
+    def holds(account_id, pagination = {}, &block)
+      get('accounts/' + account_id.to_s + '/holds' + paginate(pagination), nil, &block)
     end
 
     # Order methods
     #
-    def orders(pagination = {})
-      get('orders' + paginate(pagination))
+    def orders(pagination = {}, &block)
+      get('orders' + paginate(pagination), nil, &block)
     end
 
-    def order(order_id=nil)
+    def order(order_id=nil, &block)
       if order_id.nil? then
-        get('orders')
+        get('orders', nil, &block)
       else
-        get('orders/' + order_id.to_s)
+        get('orders/' + order_id.to_s, nil, &block)
       end
     end
 
-    def cancel_order(order_id)
-      delete('orders/' + order_id.to_s)
+    def cancel_order(order_id, &block)
+      delete('orders/' + order_id.to_s, nil, &block)
     end
 
-    def place_order(size, price, side, product_id='BTC-USD')
+    def place_order(size, price, side, product_id='BTC-USD', &block)
       order = { 'size' => size, 'price' => price, 'side' => side, 'product_id' => product_id}
-      post('orders', order)
+      post('orders', order, &block)
     end
 
     # Transfer funds methods
     #
-    def deposit(amount, coinbase_account_id)
+    def deposit(amount, coinbase_account_id, &block)
       order = { 'type' => 'deposit', 'amount' => amount, 'coinbase_account_id' => coinbase_account_id }
-      post('orders', order)
+      post('orders', order, &block)
     end
 
-    def withdraw(amount, coinbase_account_id)
+    def withdraw(amount, coinbase_account_id, &block)
       order = { 'type' => 'withdraw', 'amount' => amount, 'coinbase_account_id' => coinbase_account_id }
-      post('orders', order)
+      post('orders', order, &block)
     end
 
     # Fill methods
     #
-    def fills(pagination = {})
-      get('fills' + paginate(pagination))
+    def fills(pagination = {}, &block)
+      get('fills' + paginate(pagination), nil, &block)
     end
 
     private
