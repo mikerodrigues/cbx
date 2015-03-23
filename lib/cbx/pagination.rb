664635848f85c6a1b01f7ec21a73d246992158ee
Michael Rodrigues <mikebrodrigues@gmail.com>
2015-03-22 23:37:01 -0700

    had question mark and ampersand switched

--- lib/cbx/pagination.rb
+++ lib/cbx/pagination.rb
@@ -2,7 +2,7 @@ class CBX
   module Pagination
     private
     def paginate(hash)
-      hash.keys.each.with_index.reduce("&") {|string, (key, index)| index > 0 ? string << "?" : ""; string << "#{key}=#{hash.fetch(key)}"}
+      hash.keys.each.with_index.reduce("?") {|string, (key, index)| index > 0 ? string << "&" : ""; string << "#{key}=#{hash.fetch(key)}"}
     end
   end
 end
